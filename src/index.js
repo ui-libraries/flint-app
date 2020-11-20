72b76abc8dcd8c7181b5917da3922b22fc115b54
mbutler <matthewtbutler@gmail.com>
2020-11-19 18:25:49 -0600

    don't pop up modal if no selection

--- src/index.js
+++ src/index.js
@@ -1,48 +1,66 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
-import { db } from './database'
-import { flintIds } from './flint-ids-test'
+import {
+    db
+} from './database'
+import {
+    flintIds
+} from './flint-ids-test'
 
 const text_cloudfront = 'http://d1us66xhqwx73c.cloudfront.net/'
 const pdf_cloudfront = 'http://d3o55pxnb4jrui.cloudfront.net/'
-const document = 'deq14_b1008_3230_3230_1'
+const document = 'deq14_b1007_3229_3229_1'
 const snapshot = db.collection('pages').where('id', '==', document).get()
 snapshot.then(data => {
-  data.forEach(doc => {
-    let flintId = doc.id
-    let flintData = doc.data()
+    data.forEach(doc => {
+        let flintId = doc.id
+        let flintData = doc.data()
 
-    $('#textarea').load(text_cloudfront + document + ".txt", data => {
-      let el = window.document.getElementById('textarea')
-      el.addEventListener('mouseup', () => {
-        if (typeof window.getSelection != 'undefined') {
-            let sel = window.getSelection()
-            let selectionText = sel.toString()
-            let range = sel.getRangeAt(0)            
-            let startOffset = range.startOffset
-            let endOffset = startOffset + range.toString().length    
-            let modal = $('#selectionModal')
-            modal.find('#modalTitle').text(selectionText)
-            modal.find('.modal-body').html(`<p>Selection start: ${startOffset}</p><p>Selection end: ${endOffset}</p><label for="note">Add note:</label>
+        $('#textarea').load(text_cloudfront + document + ".txt", data => {
+            let el = window.document.getElementById('textarea')
+            el.addEventListener('mouseup', () => {
+                if (window.getSelection) {
+                    let sel = window.getSelection()
+                    let selectionText = sel.toString()
+                    let range = sel.getRangeAt(0)
+                    let startOffset = range.startOffset
+                    let endOffset = startOffset + range.toString().length
+                    let modal = $('#selectionModal')
+                    modal.find('#modalTitle').text(selectionText)
+                    modal.find('.modal-body').html(`<p>Selection start: ${startOffset}</p><p>Selection end: ${endOffset}</p><label for="note">Add note:</label>
             <input type="text" id="note" name="note">`)
-            $('#selectionModal').modal()
-            $('.btn-primary').click(e => {
-              let note = $('#note').val()
-              db.collection('pages').doc(flintId).update({
-                annotations: firebase.firestore.FieldValue.arrayUnion({
-                  selection: selectionText,
-                  start: startOffset,
-                  end: endOffset,
-                  note: note
-                })
-              })
-              modal.modal('hide')
-            })
-        }
-      }, false)
+                    if (selectionText.length > 0) {
+                        $('#selectionModal').modal()
+                        $('.btn-primary').click(e => {
+                            let note = $('#note').val()
+                            let date = new Date()
+                            console.log(date)
+                            db.collection('pages').doc(flintId).update({
+                                annotations: firebase.firestore.FieldValue.arrayUnion({
+                                    selection: selectionText,
+                                    start: startOffset,
+                                    end: endOffset,
+                                    note: note,
+                                    time: date
+                                })
+                            })
+                            modal.modal('hide')
+                        })
+                    }
+                }
+            }, false)
+        }) // jQuery load
     })
+})
 
-  })
+const myQuery = db.collection('pages').orderBy('id').startAt('deq14_b1005_3226_3226_1').limit(2).get()
+
+myQuery.then(wow => {
+    let list = []
+    wow.forEach(doc => {
+        list.push(doc.id)
+    })
+    console.log(list[1])
 })
 
 
@@ -67,8 +85,6 @@ db.collection('pages').doc('21WCLmFvBzAJGn7RIjSl').update({
   })
 })
 */
-
-
 
 
 
