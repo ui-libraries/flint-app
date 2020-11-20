7c891c1ed7693b22580540d14c3b754b4c868864
mbutler <matthewtbutler@gmail.com>
2020-11-20 16:24:20 -0600

    annotation card highlighting

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const pdf_cloudfront = 'http://d3o55pxnb4jrui.cloudfront.net/'
 
 $('#docname-input').val(getUrlDoc())
 getDocument(getUrlDoc())
-let startOffset, endOffset, selectionText, currentId
+let startOffset, endOffset, selectionText, currentId, mainText
 
 $('#doc-submit').click(e => {
     let document = $('#docname-input').val()
@@ -31,6 +31,7 @@ function getDocument(document) {
             currentId = doc.id
             let flintData = doc.data()
             $('#textarea').load(text_cloudfront + document + ".txt", data => {
+              mainText = $('#textarea').html()
                 $("#pdf").attr("href", pdf_cloudfront + document + ".pdf")
                 let el = window.document.getElementById('textarea')
                 el.addEventListener('mouseup', () => {
@@ -86,6 +87,25 @@ $('#next').click(e => {
     })
 })
 
+$('#annotations').on('click', '.card', e => {
+  $( "#annotations" ).children().css( 'box-shadow', '0px 0px 0px #888')
+  $(e.currentTarget).css('box-shadow', '10px 10px 5px #888')
+ let content = $( e.currentTarget ).find( "h6" ).html()
+ let matches = content.match(/(\d+)/g)
+ highlightSelection(matches)
+})
+
+function highlightSelection(rangeNumbers) {
+  $('#textarea').empty().html(mainText)
+  const element = document.getElementById('textarea')
+  const textNode = element.childNodes[0]
+  const range = window.document.createRange()
+  range.setStart(textNode, rangeNumbers[0])
+  range.setEnd(textNode, rangeNumbers[1])
+  const mark = document.createElement('mark')
+  range.surroundContents(mark)
+}
+
 function cleanId(document) {
     let doc
     doc = document.substring(document.lastIndexOf("/") + 1)
@@ -116,7 +136,7 @@ function displayAnnotationCards(annotations) {
   <h4 class="card-title">${item.selection}</h4>
   <h6 class="card-subtitle mb-2 text-muted">start: ${item.start} end: ${item.end}</h6>
   <h5 class="card-text">${item.note}</h5>
-  <h6 class="card-subtitle mb-2 text-muted">created: ${time}</h6>
+  <p">created: ${time}</p>
 </div>
 </div>`
             $('#annotations').append(html)
